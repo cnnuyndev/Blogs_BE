2320e7739da02a40b363e2191e3960357807ff7a
cnnuyndev <cn.nguyen.dev@gmail.com>
2025-08-21 20:39:04 +0700

    create static for whitenoise

--- BE_BLOG/settings.py
+++ BE_BLOG/settings.py
@@ -128,10 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/"static"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-MEDIA_URL = "img/"
+MEDIA_URL = "/img/"
 MEDIA_ROOT = BASE_DIR/"media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
